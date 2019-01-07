055e8bf28ee153a6bc755ddfc10b9a2848fcb1a1
Alyssa Foxx <alyssamariefoxx@gmail.com>
2019-01-07 01:49:29 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,13 +1,13 @@
 class Post 
   attr_accessor :title, :author
-   @@all = self
    
   def initialize(title)
     @title = title
   end
    
   def author_name
-    self.author.name
+    if self.author.name == nil
+      
   end
 end 
 
